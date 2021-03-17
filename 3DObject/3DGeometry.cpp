de896254ddeb264e4712a892fee3035ee8eef2ec
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-03-17 22:08:16 +0100

    Just formatting

--- 3DObject/3DGeometry.cpp
+++ 3DObject/3DGeometry.cpp
@@ -1,20 +1,20 @@
 #include "3DGeometry.h"
 
-doubli sqr(const doubli &d) { return d*d; }
-radiant degreesToRadians(const doubli &deg) { return fmodl(deg, 360) * doubli(M_PI) / 180; }
-doubli radiansToDegrees(const radiant &rad) { return rad * 180 / doubli(M_PI); }
-doubli sqrt(const doubli &d) { return std::sqrt(d); }
-doubli cos(const radiant &deg) { return std::cos(deg); }
-doubli sin(const radiant &deg) { return std::sin(deg); }
-doubli tan(const radiant &deg) { return std::tan(deg); }
-radiant acos(const doubli &d) { return std::acos(d); }
-radiant asin(const doubli &d) { return std::asin(d); }
-radiant atan(const doubli &d) { return std::atan(d); }
-doubli qIsInf(const doubli &d) { return qIsInf(static_cast<double>(d)); }
-QDebug operator <<(QDebug debug, const doubli &d) { debug << static_cast<double>(d); return debug; }
+doubli sqr(const doubli& d) { return d * d; }
+radiant degreesToRadians(const doubli& deg) { return fmodl(deg, 360) * doubli(M_PI) / 180; }
+doubli radiansToDegrees(const radiant& rad) { return rad * 180 / doubli(M_PI); }
+doubli sqrt(const doubli& d) { return std::sqrt(d); }
+doubli cos(const radiant& deg) { return std::cos(deg); }
+doubli sin(const radiant& deg) { return std::sin(deg); }
+doubli tan(const radiant& deg) { return std::tan(deg); }
+radiant acos(const doubli& d) { return std::acos(d); }
+radiant asin(const doubli& d) { return std::asin(d); }
+radiant atan(const doubli& d) { return std::atan(d); }
+doubli qIsInf(const doubli& d) { return qIsInf(static_cast<double>(d)); }
+QDebug operator <<(QDebug debug, const doubli& d) { debug << static_cast<double>(d); return debug; }
 
 
-Point3D::Point3D() { x=0; y=0; z=0; }
+Point3D::Point3D() { x = 0; y = 0; z = 0; }
 Point3D::Point3D(doubli x, doubli y, doubli z)
 {
     this->x = round(x);
@@ -22,14 +22,14 @@ Point3D::Point3D(doubli x, doubli y, doubli z)
     this->z = round(z);
     defined = true;
 }
-Point3D::Point3D(const Point3D &point)
+Point3D::Point3D(const Point3D& point)
 {
     x = point.x;
     y = point.y;
     z = point.z;
     defined = point.defined;
 }
-Point3D *Point3D::operator =(const Point3D &point)
+Point3D* Point3D::operator =(const Point3D& point)
 {
     x = point.x;
     y = point.y;
@@ -38,77 +38,83 @@ Point3D *Point3D::operator =(const Point3D &point)
     return this;
 }
 
-bool Point3D::operator <(const Point3D &point) const
+bool Point3D::operator <(const Point3D& point) const
 {
     //le plus petit = la plus proche
-    if(z == point.z)
+    if (z == point.z)
         return distanceAxeZ() < point.distanceAxeZ();//si plus proche axe Z => plus proche
     else
         return z < point.z;//si plus proche Z alors plus proche
 }
-bool Point3D::operator >(const Point3D &point) const
+bool Point3D::operator >(const Point3D& point) const
 {
     //le plus grand = la plus loins
-    if(z == point.z)
+    if (z == point.z)
         return distanceAxeZ() > point.distanceAxeZ();//si plus loins axe Z => plus loins
     else
         return z > point.z;//si plus loins Z alors plus loins
 }
-bool Point3D::operator ==(const Point3D &point) const
-{ return (x == point.x) && (y == point.y) && (z == point.z); }
+bool Point3D::operator ==(const Point3D& point) const
+{
+    return (x == point.x) && (y == point.y) && (z == point.z);
+}
 
-Point3D Point3D::operator +(const Point3D &point) const
+Point3D Point3D::operator +(const Point3D& point) const
 {
-    return Point3D(x+point.x,
-                   y+point.y,
-                   z+point.z);
+    return Point3D(x + point.x,
+        y + point.y,
+        z + point.z);
 }
-Point3D Point3D::operator -(const Point3D &point) const
+Point3D Point3D::operator -(const Point3D& point) const
 {
-    return Point3D(x-point.x,
-                   y-point.y,
-                   z-point.z);
+    return Point3D(x - point.x,
+        y - point.y,
+        z - point.z);
 }
-Point3D Point3D::operator *(const Point3D &point) const
+Point3D Point3D::operator *(const Point3D& point) const
 {
-    return Point3D(x*point.x,
-                   y*point.y,
-                   z*point.z);
+    return Point3D(x * point.x,
+        y * point.y,
+        z * point.z);
 }
-Point3D Point3D::operator *(doubli n) const { return Point3D(x*n, y*n, z*n); }
-Point3D Point3D::operator /(const Point3D &point) const
+Point3D Point3D::operator *(doubli n) const { return Point3D(x * n, y * n, z * n); }
+Point3D Point3D::operator /(const Point3D& point) const
 {
-    return Point3D(x/point.x,
-                   y/point.y,
-                   z/point.z);
+    return Point3D(x / point.x,
+        y / point.y,
+        z / point.z);
 }
-Point3D Point3D::operator /(doubli n) const { return Point3D(x/n, y/n, z/n); }
+Point3D Point3D::operator /(doubli n) const { return Point3D(x / n, y / n, z / n); }
 
-doubli Point3D::distance(const Point3D &pA, const Point3D &pB)
+doubli Point3D::distance(const Point3D& pA, const Point3D& pB)
 {
-    return sqrt(sqr(pB.x - pA.x)+
-                sqr(pB.y - pA.y)+
-                sqr(pB.z - pA.z));
+    return sqrt(sqr(pB.x - pA.x) +
+        sqr(pB.y - pA.y) +
+        sqr(pB.z - pA.z));
 }
-doubli Point3D::distanceMax(const Point3D &pA, const Point3D &pB)
+doubli Point3D::distanceMax(const Point3D& pA, const Point3D& pB)
 {
     doubli dX = abs(pB.x - pA.x),
-           dY = abs(pB.y - pA.y),
-           dZ = abs(pB.z - pA.z);
-    if(dX < dY)
+        dY = abs(pB.y - pA.y),
+        dZ = abs(pB.z - pA.z);
+    if (dX < dY)
         dX = dY;
     return dX < dZ ? dZ : dX;//on prend le plus grand (ça forme un carré)
 }
 
-QDebug operator <<(QDebug debug, const Point3D &point)
+QDebug operator <<(QDebug debug, const Point3D& point)
 {
     debug << "Point3D(" << point.x << "," << point.y << "," << point.z << ")";
     return debug;
 }
-Point3D qFloor(const Point3D &point)
-{ return Point3D(std::floor(point.x), std::floor(point.y), std::floor(point.z)); }
-Point3D qCeil(const Point3D &point)
-{ return Point3D(std::ceil(point.x), std::ceil(point.y), std::ceil(point.z)); }
+Point3D qFloor(const Point3D& point)
+{
+    return Point3D(std::floor(point.x), std::floor(point.y), std::floor(point.z));
+}
+Point3D qCeil(const Point3D& point)
+{
+    return Point3D(std::ceil(point.x), std::ceil(point.y), std::ceil(point.z));
+}
 
 
 
@@ -124,9 +130,9 @@ Pos3D::Pos3D(const Point3D& point, radiant rX, radiant rZ) : Point3D(point)
 {
     this->rX = rX; this->rZ = rZ;
 }
-Pos3D::Pos3D(const Pos3D &pos) : Point3D(pos) { rX = pos.rX; rZ = pos.rZ; }
+Pos3D::Pos3D(const Pos3D& pos) : Point3D(pos) { rX = pos.rX; rZ = pos.rZ; }
 
-Pos3D *Pos3D::operator=(const Pos3D &pos)
+Pos3D* Pos3D::operator=(const Pos3D& pos)
 {
     Point3D::operator=(pos);
     rX = pos.rX;
@@ -142,27 +148,27 @@ Pos3D Pos3D::fromDegree(doubli x, doubli y, doubli z, radiant rX, radiant rZ)
 void Pos3D::moveWithRot(doubli speed, radiant rot)
 {
     //trigo, on connait l'hypothénuse (speed) et l'angle et on veut coté a ou b => cos ou sin
-    addX(cos(rX+rot) * speed);
-    addY(sin(rX+rot) * speed);
+    addX(cos(rX + rot) * speed);
+    addY(sin(rX + rot) * speed);
 }
 
 Point3D Pos3D::pointFromRot(doubli d, radiant rX, radiant rZ)
 {
-    return Point3D(cos(rZ)*cos(rX),
-                   cos(rZ)*sin(rX),
-                   sin(rZ) * d) * d;
+    return Point3D(cos(rZ) * cos(rX),
+        cos(rZ) * sin(rX),
+        sin(rZ) * d) * d;
 }
 Point3D Pos3D::getNextPointRelatif() const
 {
     //on s'avance de 1 t
     //équivalent à pointFromRot(1, rX, rZ);
-    return Point3D(cos(rZ)*cos(rX), cos(rZ)*sin(rX), sin(rZ));
+    return Point3D(cos(rZ) * cos(rX), cos(rZ) * sin(rX), sin(rZ));
 }
 Point3D Pos3D::getNextPoint() const
 {
-    return Point3D(cos(rZ)*cos(rX) + getX(),
-                   cos(rZ)*sin(rX) + getY(),
-                   sin(rZ) + getZ());
+    return Point3D(cos(rZ) * cos(rX) + getX(),
+        cos(rZ) * sin(rX) + getY(),
+        sin(rZ) + getZ());
     //opti mais == getNextPointRelatif() + getPoint()
 }
 
@@ -170,22 +176,22 @@ Pos3D Pos3D::getChildRot(radiant rXRelatif, radiant rZRelatif) const
 {
     //changeRef de childNext
     doubli crZR = cos(rZRelatif),
-            crZ = cos(rZ), crX = cos(rX),
-            srZ = sin(rZ), srX = sin(rX);
-    doubli x1 = crZR*cos(rXRelatif), y1 = crZR*sin(rXRelatif), z1 = sin(rZRelatif);//z1==srZR
-    doubli x2 = x1*crZ - z1*srZ, z2 = x1*srZ + z1*crZ;//y2==y1
-    doubli x3 = x2*crX - y1*srX, y3 = x2*srX + y1*crX;//z3==z2
-    radiant rX = atan(y3/x3);
-    if(x3 < 0) {//sinon on ne voit que tout ce qui est en x3>=0 (rotation de 180° du reste)
+        crZ = cos(rZ), crX = cos(rX),
+        srZ = sin(rZ), srX = sin(rX);
+    doubli x1 = crZR * cos(rXRelatif), y1 = crZR * sin(rXRelatif), z1 = sin(rZRelatif);//z1==srZR
+    doubli x2 = x1 * crZ - z1 * srZ, z2 = x1 * srZ + z1 * crZ;//y2==y1
+    doubli x3 = x2 * crX - y1 * srX, y3 = x2 * srX + y1 * crX;//z3==z2
+    radiant rX = atan(y3 / x3);
+    if (x3 < 0) {//sinon on ne voit que tout ce qui est en x3>=0 (rotation de 180° du reste)
         rX += M_PI;//atan retourne -89.5 alors qu'il faut 90.5 (mais ça reste la meme chose)
         //pas besoin de différentier y car on est à k360° de diff
     }
     return Pos3D(getPoint(), rX, asin(z2));
 }
 
-Point3D Pos3D::changeRef(const Point3D &point) const { return rotation(point + getPoint(), rX, rZ); }
+Point3D Pos3D::changeRef(const Point3D& point) const { return rotation(point + getPoint(), rX, rZ); }
 
-bool Pos3D::operator ==(const Pos3D &pos) const
+bool Pos3D::operator ==(const Pos3D& pos) const
 {
     return Point3D::operator ==(static_cast<Point3D>(pos)) && rX == pos.rX && rZ == pos.rZ;
 }
@@ -198,14 +204,14 @@ Point3D Pos3D::rotation(Point3D point, radiant rX, radiant rZ)
     sR = sin(rX); cR = cos(rX);
     return Point3D(rotation1(point.getX(), point.getY(), sR, cR), rotation2(point.getX(), point.getY(), sR, cR), point.getZ());
 }
-doubli Pos3D::rotation1(doubli x, doubli y, doubli sR, doubli cR) { return x*cR - y*sR; }
-doubli Pos3D::rotation2(doubli x, doubli y, doubli sR, doubli cR) { return x*sR + y*cR; }
-Pos3D Pos3D::getRotAsVect(const Point3D &p1, const Point3D &p2)
+doubli Pos3D::rotation1(doubli x, doubli y, doubli sR, doubli cR) { return x * cR - y * sR; }
+doubli Pos3D::rotation2(doubli x, doubli y, doubli sR, doubli cR) { return x * sR + y * cR; }
+Pos3D Pos3D::getRotAsVect(const Point3D& p1, const Point3D& p2)
 {
-    radiant rZ = asin((p2.getZ()-p1.getZ())/Point3D::distance(p1, p2));//formule getNextPos z inversée
-    radiant rX = atan((p2.getY()-p1.getY())/(p2.getX()-p1.getX()));
-    if(p2.getX()-p1.getX() < 0) {
-        if(p2.getY()-p1.getY() >= 0)
+    radiant rZ = asin((p2.getZ() - p1.getZ()) / Point3D::distance(p1, p2));//formule getNextPos z inversée
+    radiant rX = atan((p2.getY() - p1.getY()) / (p2.getX() - p1.getX()));
+    if (p2.getX() - p1.getX() < 0) {
+        if (p2.getY() - p1.getY() >= 0)
             rX += M_PI;//atan retourne -89.5 alors qu'il faut 90.5 (mais ça reste la meme chose)
         else
             rX -= M_PI;
@@ -213,14 +219,14 @@ Pos3D Pos3D::getRotAsVect(const Point3D &p1, const Point3D &p2)
     return Pos3D(p1, rX, rZ);
 }
 
-Pos3D Pos3D::getRotAsPoint(const Point3D &p)
+Pos3D Pos3D::getRotAsPoint(const Point3D& p)
 {
     //opti mais == getRotAsVect(Point3D(0,0,0),p)
-    doubli d = Point3D::distance(Point3D(0,0,0), p);
-    radiant rZ = asin((p.getZ())/d);//formule getNextPos z inversée
-    radiant rX = atan((p.getY())/(p.getX()));
-    if(p.getX() < 0) {
-        if(p.getY() >= 0)
+    doubli d = Point3D::distance(Point3D(0, 0, 0), p);
+    radiant rZ = asin((p.getZ()) / d);//formule getNextPos z inversée
+    radiant rX = atan((p.getY()) / (p.getX()));
+    if (p.getX() < 0) {
+        if (p.getY() >= 0)
             rX += M_PI;//atan retourne -89.5 alors qu'il faut 90.5 (mais ça reste la meme chose)
         else
             rX -= M_PI;
@@ -229,55 +235,67 @@ Pos3D Pos3D::getRotAsPoint(const Point3D &p)
 }
 
 
-QDebug operator <<(QDebug debug, const Pos3D &pos)
+QDebug operator <<(QDebug debug, const Pos3D& pos)
 {
     debug << "Pos3D(" << pos.getX() << "," << pos.getY() << "," << pos.getZ()
-          << ";" << pos.getRX() << "," << pos.getRZ() << ")";
+        << ";" << pos.getRX() << "," << pos.getRZ() << ")";
     return debug;
 }
 
 
 
 Size3D::Size3D(doubli width, doubli height, doubli depth)
-{ this->width = width; this->height = height; this->depth = depth; }
-Size3D::Size3D(const Size3D &size)
-{ width = size.width; height = size.height; depth = size.depth; }
-Size3D::Size3D(const Point3D &pA, const Point3D &pB)
+{
+    this->width = width; this->height = height; this->depth = depth;
+}
+Size3D::Size3D(const Size3D& size)
+{
+    width = size.width; height = size.height; depth = size.depth;
+}
+Size3D::Size3D(const Point3D& pA, const Point3D& pB)
 {
     width = pB.getX() - pA.getX();
     height = pB.getY() - pA.getY();
     depth = pB.getZ() - pA.getZ();
 }
-Point3D operator -(const Point3D &p, const Size3D &s)
-{ return Point3D(p.getX()-s.getWidth(), p.getY()-s.getHeight(), p.getZ()-s.getDepth()); }
-Point3D operator +(const Point3D &p, const Size3D &s)
-{ return Point3D(p.getX()+s.getWidth(), p.getY()+s.getHeight(), p.getZ()+s.getDepth()); }
+Point3D operator -(const Point3D& p, const Size3D& s)
+{
+    return Point3D(p.getX() - s.getWidth(), p.getY() - s.getHeight(), p.getZ() - s.getDepth());
+}
+Point3D operator +(const Point3D& p, const Size3D& s)
+{
+    return Point3D(p.getX() + s.getWidth(), p.getY() + s.getHeight(), p.getZ() + s.getDepth());
+}
 
 Rect3D::Rect3D()
 {
     pointMin = Point3D(0, 0, 0);
     pointMax = Point3D(0, 0, 0);
 }
-Rect3D::Rect3D(const Point3D &pointA, const Point3D &pointB)
+Rect3D::Rect3D(const Point3D& pointA, const Point3D& pointB)
 {
     pointMin = pointA; pointMax = pointB;
-    if(pointB.getX() < pointA.getX()) {
+    if (pointB.getX() < pointA.getX()) {
         pointMin.setX(pointB.getX());
         pointMax.setX(pointA.getX());
     }
-    if(pointB.getY() < pointA.getY()) {
+    if (pointB.getY() < pointA.getY()) {
         pointMin.setY(pointB.getY());
         pointMax.setY(pointA.getY());
     }
-    if(pointB.getZ() < pointA.getZ()) {
+    if (pointB.getZ() < pointA.getZ()) {
         pointMin.setZ(pointB.getZ());
         pointMax.setZ(pointA.getZ());
     }
 }
-Rect3D::Rect3D(const Point3D &pointA, const Size3D &size)
-{ pointMin = pointA; pointMax = pointA + size; }
-Rect3D::Rect3D(const Rect3D &rect)
-{ pointMin = rect.pointMin; pointMax = rect.pointMax; }
+Rect3D::Rect3D(const Point3D& pointA, const Size3D& size)
+{
+    pointMin = pointA; pointMax = pointA + size;
+}
+Rect3D::Rect3D(const Rect3D& rect)
+{
+    pointMin = rect.pointMin; pointMax = rect.pointMax;
+}
 
 void Rect3D::scale(double scale)
 {
@@ -286,18 +304,18 @@ void Rect3D::scale(double scale)
     pointMax = getMiddle() + demisizeScaled;
 }
 
-bool Rect3D::contains(const Point3D &point) const
+bool Rect3D::contains(const Point3D& point) const
 {
-    if(point.getX() < pointMin.getX() || pointMax.getX() < point.getX())
+    if (point.getX() < pointMin.getX() || pointMax.getX() < point.getX())
         return false;
-    if(point.getY() < pointMin.getY() || pointMax.getY() < point.getY())
+    if (point.getY() < pointMin.getY() || pointMax.getY() < point.getY())
         return false;
-    if(point.getZ() < pointMin.getZ() || pointMax.getZ() < point.getZ())
+    if (point.getZ() < pointMin.getZ() || pointMax.getZ() < point.getZ())
         return false;
     return true;
 }
 
-QDebug operator <<(QDebug debug, const Rect3D &rect)
+QDebug operator <<(QDebug debug, const Rect3D& rect)
 {
     debug << "Rect3D(" << rect.getPointMin() << "->" << rect.getPointMax() << ")";
     return debug;
--- 3DObject/3DGeometry.h
+++ 3DObject/3DGeometry.h
@@ -8,28 +8,28 @@
 
 
 typedef long double doubli;//un nombre pour gérer 6 décimales max (arrondit)
-static doubli round(const doubli &d) { return std::floor(d*1000000+0.5L)/1000000; }
+static doubli round(const doubli& d) { return std::floor(d * 1000000 + 0.5L) / 1000000; }
 typedef float radiant;
-doubli sqr(const doubli &d);//easy
-radiant degreesToRadians(const doubli &deg);
-doubli radiansToDegrees(const radiant &rad);
-doubli sqrt(const doubli &d);
-doubli cos(const radiant &deg);
-doubli sin(const radiant &deg);
-doubli tan(const radiant &deg);
-radiant acos(const doubli &d);
-radiant asin(const doubli &d);
-radiant atan(const doubli &d);
-doubli qIsInf(const doubli &d);
-QDebug operator <<(QDebug debug, const doubli &d);
+doubli sqr(const doubli& d);//easy
+radiant degreesToRadians(const doubli& deg);
+doubli radiansToDegrees(const radiant& rad);
+doubli sqrt(const doubli& d);
+doubli cos(const radiant& deg);
+doubli sin(const radiant& deg);
+doubli tan(const radiant& deg);
+radiant acos(const doubli& d);
+radiant asin(const doubli& d);
+radiant atan(const doubli& d);
+doubli qIsInf(const doubli& d);
+QDebug operator <<(QDebug debug, const doubli& d);
 
 class Point3D
 {
 public:
     Point3D();
     Point3D(doubli x, doubli y, doubli z);
-    Point3D(const Point3D &point);
-    Point3D *operator =(const Point3D &point);
+    Point3D(const Point3D& point);
+    Point3D* operator =(const Point3D& point);
 
     doubli getX() const { return x; }
     doubli getY() const { return y; }
@@ -38,29 +38,29 @@ public:
     void setX(doubli x) { this->x = round(x); }
     void setY(doubli y) { this->y = round(y); }
     void setZ(doubli z) { this->z = round(z); }
-    void setPoint(const Point3D &point) { operator=(point); }
+    void setPoint(const Point3D& point) { operator=(point); }
     void addX(doubli x) { this->x = round(this->x + x); }
     void addY(doubli y) { this->y = round(this->y + y); }
     void addZ(doubli z) { this->z = round(this->z + z); }
 
-    doubli distanceAxeZ() const { return std::sqrt(x*x + y*y); }
-    bool operator <(const Point3D &point) const;
-    bool operator >(const Point3D &point) const;
-    bool operator ==(const Point3D &point) const;
-    bool isNull() const { return x==0.0L && y==0.0L && z==0.0L; }
+    doubli distanceAxeZ() const { return std::sqrt(x * x + y * y); }
+    bool operator <(const Point3D& point) const;
+    bool operator >(const Point3D& point) const;
+    bool operator ==(const Point3D& point) const;
+    bool isNull() const { return x == 0.0L && y == 0.0L && z == 0.0L; }
     bool isValid() const { return defined; }
     bool isInf() const { return qIsInf(x) || qIsInf(y) || qIsInf(z); }
     static Point3D inifinite() { return Point3D(INFINITY, INFINITY, INFINITY); }
 
-    Point3D operator +(const Point3D &point) const;
-    Point3D operator -(const Point3D &point) const;
-    Point3D operator *(const Point3D &point) const;
+    Point3D operator +(const Point3D& point) const;
+    Point3D operator -(const Point3D& point) const;
+    Point3D operator *(const Point3D& point) const;
     Point3D operator *(doubli n) const;
-    Point3D operator /(const Point3D &point) const;
+    Point3D operator /(const Point3D& point) const;
     Point3D operator /(doubli n) const;
 
-    static doubli distance(const Point3D &pA, const Point3D &pB);
-    static doubli distanceMax(const Point3D &pA, const Point3D &pB);
+    static doubli distance(const Point3D& pA, const Point3D& pB);
+    static doubli distanceMax(const Point3D& pA, const Point3D& pB);
 
     friend QDebug operator <<(QDebug debug, const Point3D& point);
     friend Point3D qFloor(const Point3D& point);
@@ -77,9 +77,9 @@ class Pos3D : public Point3D {
 public:
     Pos3D();
     Pos3D(doubli x, doubli y, doubli z, radiant rX, radiant rZ);
-    Pos3D(const Point3D &point, radiant rX, radiant rZ);
-    Pos3D(const Pos3D &pos);
-    Pos3D *operator=(const Pos3D &pos);
+    Pos3D(const Point3D& point, radiant rX, radiant rZ);
+    Pos3D(const Pos3D& pos);
+    Pos3D* operator=(const Pos3D& pos);
     static Pos3D fromDegree(doubli x, doubli y, doubli z, radiant rX, radiant rZ);
 
     radiant getRX() const { return rX; }
@@ -87,7 +87,7 @@ public:
     Pos3D getPos() const { return *this; }
     void setRX(radiant rX) { this->rX = rX; }
     void setRZ(radiant rZ) { this->rZ = rZ; }
-    void setPos(const Pos3D &pos) { operator=(pos); }
+    void setPos(const Pos3D& pos) { operator=(pos); }
     void addRX(radiant rX) { this->rX += rX; }
     void addRZ(radiant rZ) { this->rZ += rZ; }
 
@@ -96,10 +96,10 @@ public:
     Point3D getNextPointRelatif() const;
     Point3D getNextPoint() const;
     Pos3D getChildRot(radiant rXRelatif, radiant rZRelatif) const;
-    Point3D changeRef(const Point3D &point) const;
-    bool operator ==(const Pos3D &pos) const;
+    Point3D changeRef(const Point3D& point) const;
+    bool operator ==(const Pos3D& pos) const;
 
-    friend QDebug operator << (QDebug debug, const Pos3D &pos);
+    friend QDebug operator << (QDebug debug, const Pos3D& pos);
 private:
     radiant rX;
     radiant rZ;
@@ -107,8 +107,8 @@ private:
     static Point3D rotation(Point3D point, radiant rX, radiant rZ);
     static doubli rotation1(doubli x, doubli y, doubli sR, doubli cR);
     static doubli rotation2(doubli x, doubli y, doubli sR, doubli cR);
-    static Pos3D getRotAsVect(const Point3D &p1, const Point3D &p2);
-    static Pos3D getRotAsPoint(const Point3D &p);
+    static Pos3D getRotAsVect(const Point3D& p1, const Point3D& p2);
+    static Pos3D getRotAsPoint(const Point3D& p);
 
 };
 
@@ -117,8 +117,8 @@ class Size3D
 public:
     Size3D();
     Size3D(doubli width, doubli height, doubli depth);
-    Size3D(const Size3D &size);
-    Size3D(const Point3D &pA, const Point3D &pB);
+    Size3D(const Size3D& size);
+    Size3D(const Point3D& pA, const Point3D& pB);
     doubli getWidth() const { return width; }
     doubli getHeight() const { return height; }
     doubli getDepth() const { return depth; }
@@ -126,23 +126,23 @@ public:
     void setWidth(doubli width) { this->width = round(width); }
     void setHeight(doubli height) { this->height = round(height); }
     void setDepth(doubli depth) { this->depth = round(depth); }
-    Size3D operator *(doubli scale) const { return Size3D(width*scale, height*scale, depth*scale); }
-    Size3D operator /(doubli scale) const { return Size3D(width/scale, height/scale, depth/scale); }
+    Size3D operator *(doubli scale) const { return Size3D(width * scale, height * scale, depth * scale); }
+    Size3D operator /(doubli scale) const { return Size3D(width / scale, height / scale, depth / scale); }
 protected:
     doubli width;//X
     doubli height;//Y
     doubli depth;//Z
 };
-Point3D operator -(const Point3D &p, const Size3D &s);
-Point3D operator +(const Point3D &p, const Size3D &s);
+Point3D operator -(const Point3D& p, const Size3D& s);
+Point3D operator +(const Point3D& p, const Size3D& s);
 
 class Rect3D
 {
 public:
     Rect3D();
-    Rect3D(const Point3D &pointA, const Point3D &pointB);
-    Rect3D(const Point3D &pointA, const Size3D &size);
-    Rect3D(const Rect3D &rect);
+    Rect3D(const Point3D& pointA, const Point3D& pointB);
+    Rect3D(const Point3D& pointA, const Size3D& size);
+    Rect3D(const Rect3D& rect);
     Size3D getSize() const { return Size3D(pointMin, pointMax); }
     Point3D getPointMin() const { return pointMin; }
     Point3D getPointMax() const { return pointMax; }
@@ -151,18 +151,18 @@ public:
     //void setPointMin(const Point3D &pointMin) { this->pointMin = pointMin; }//pas recommandé
     //void setPointMax(const Point3D &pointMax) { this->pointMax = pointMax; }//pas recommandé
     void scale(double scale);
-    Rect3D operator +(const Point3D &pointMinAdd) const { return Rect3D(pointMin+pointMinAdd, getSize()); }
-    Rect3D *operator =(const Rect3D &rect) { pointMin=rect.pointMin; pointMax=rect.pointMax; return this; }
-    bool operator ==(const Rect3D &rect) const { return pointMin == rect.pointMin && pointMax == rect.pointMax; }
+    Rect3D operator +(const Point3D& pointMinAdd) const { return Rect3D(pointMin + pointMinAdd, getSize()); }
+    Rect3D* operator =(const Rect3D& rect) { pointMin = rect.pointMin; pointMax = rect.pointMax; return this; }
+    bool operator ==(const Rect3D& rect) const { return pointMin == rect.pointMin && pointMax == rect.pointMax; }
 
-    bool contains(const Point3D &point) const;
+    bool contains(const Point3D& point) const;
 
 private:
     Point3D pointMin;
     Point3D pointMax;
     //TODO pb: on a pas assez d'info, la on a qu'une droite de modélisé, pas du tout un rectangle
 };
-QDebug operator << (QDebug debug, const Rect3D &rect);
+QDebug operator << (QDebug debug, const Rect3D& rect);
 
 
 
