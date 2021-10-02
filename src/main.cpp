05374383aca92db3a91276a72af7b4b36b246d92
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-10-02 11:58:56 +0200

    CreateConsole for Visual Studio (def in preproc)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 
 #include "GUI/fenetre.h"
 
+bool CreateConsole()
+{
+    // https://stackoverflow.com/a/57241985/12908345
+    if (!AttachConsole(ATTACH_PARENT_PROCESS) && !AllocConsole()) {
+        return false;
+    }
+
+    FILE *fDummy;
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+    std::cout.clear();
+    std::clog.clear();
+    std::cerr.clear();
+    std::cin.clear();
+
+    return true;
+}
+
 class Application
 {
 public:
@@ -27,6 +46,9 @@ private:
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+#ifdef _WINDOWS
+    if (!CreateConsole()) return -1;
+#endif
     std::cout << "[WinMain] starting..." << std::endl;
     Application app(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
     std::cout << "[WinMain] Window created!" << std::endl;
